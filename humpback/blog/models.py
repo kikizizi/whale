fb717012a34f2a409cb8585933b9a226b236b8a1
coolseaweed <jeellato@gmail.com>
2022-04-24 17:03:31 +0900

    사진업로드 출력

--- humpback/blog/models.py
+++ humpback/blog/models.py
@@ -5,6 +5,6 @@ class Post(models.Model):
     content=models.TextField()
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
-
+    head_image=models.ImageField(upload_to='blog/images/%Y/%m/%d/',blank=True)
     def get_absulute_url(self):
         return f'/blog/{self.pk}/'
