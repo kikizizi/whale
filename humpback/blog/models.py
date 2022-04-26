0126430eb4e16cf8bed6ab124955bffcf02d66dc
kikizizi <du917@naver.com>
2022-04-27 00:51:43 +0900

    파일편의성확장 임시 주석처리

--- humpback/blog/models.py
+++ humpback/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import os
 
 class Post(models.Model):
     title=models.CharField(max_length=100)
@@ -9,3 +10,10 @@ class Post(models.Model):
     file_upload=models.FileField(upload_to='blog/file/%Y/%m/%d/',blank=True)
     def get_absulute_url(self):
         return f'/blog/{self.pk}/'
+
+    # 파일명얻기
+    # def get_file_name(self):
+    #     return os.path.basename(self.file_upload.name)
+    # 파일 확장자 얻기
+    # def get_file_ext(self):
+    #     return self.get_file_name().split('.')[-1]
